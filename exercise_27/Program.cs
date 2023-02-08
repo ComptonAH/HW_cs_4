12c23e3e1e483388a287b7ed5a3e889ab882e339
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-08 23:30:52 +0300

    exercise 27 had been added

---
+++ exercise_27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Enter a number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int length = Convert.ToInt32(Convert.ToString(number).Length);
+int[] Array = new int[number];
+int sum = 0; 
+
+
+for (int i = 0; i <= length; i++)
+{
+    if (i==length)
+    {
+        for (int index = 0; index < length; index++)
+        {
+            sum = sum + Array[index];
+        }
+    }
+    else
+    {
+    int digit = number%10;
+    number = number/10;
+    Array[i] = digit;
+    }
+}
+
+Console.WriteLine(sum);
