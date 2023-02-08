26a874e41f960322c5b678dce23e917b19f7dbda
NIkita Karetkin <nikita-kareta@yandex.ru>
2023-02-08 13:11:48 +0300

    Exercise 25 had been

---
+++ exercise_25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number you want to power: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the power of your number: ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1;
+
+for (int i = 1; i < num2; i++)
+{
+    result = result*num1;
+}
+Console.WriteLine(result);
