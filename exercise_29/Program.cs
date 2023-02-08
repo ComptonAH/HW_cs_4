2dc25561640b1673abf51e37995df39dbc86a9b0
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-08 23:58:26 +0300

    exercise 29 had been added

---
+++ exercise_29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Enter number of elements of an array: ");
+int elements = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[elements];
+
+for (int i = 0; i < elements; i++)
+{
+    Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+foreach (int digits in Array) Console.Write($"{digits} ");
